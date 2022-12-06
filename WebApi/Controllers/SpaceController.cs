865d27be3887359fb078f0a8f5fecbaf9c06dbc8
adinamolad <adinamolad@gmail.com>
2022-12-06 21:10:41 +0200

    first commit

---
+++ WebApi/Controllers/SpaceController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpaceController : ControllerBase
+    {
+        public static List<LocationNote> _locationNotes = new List<LocationNote>();
+
+
+        [HttpPost]
+        public IEnumerable<LocationNote> Post(LocationNote locationNote)
+        {
+            _locationNotes.Add(locationNote);
+            return _locationNotes.ToArray();
+        }
+
+       
+
+
+    }
+}
---
+++ WebApi/Models/LocationNote.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Models
+{
+    public class LocationNote
+    {
+        public string Note { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+
+        public LocationNote(string note, decimal longitude, decimal latitude)
+        {
+            Note = note;
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+    }
+}
